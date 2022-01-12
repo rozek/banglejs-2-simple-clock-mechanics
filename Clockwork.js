c07731304cb2e760cf8761c8c6568d5b72c197bd
Andreas Rozek <A.Rozek@gmx.de>
2022-01-12 17:11:51 +0100

    Update Clockwork.js

--- Clockwork.js
+++ Clockwork.js
@@ -21,18 +21,24 @@
 
     let Timer;
     function refreshDisplay () {
+      Timer = undefined;
+
       g.reset();
       if (Background == null) {
         g.setBgColor(Settings.Background === 'Theme' ? g.theme.bg : Settings.Background || '#FFFFFF');
         g.clear(false);
       } else {
-        Background.draw(Settings);
+        try {
+          Background.draw(Settings);
+        } catch (Signal) { print('Error drawing background: ',Signal); }
       }
 
       Bangle.drawWidgets();
 
       if (ClockFace != null) {
-        ClockFace.draw(Settings, CenterX,CenterY, outerRadius);
+        try {
+          ClockFace.draw(Settings, CenterX,CenterY, outerRadius);
+        } catch (Signal) { print('Error drawing clock face: ',Signal); }
       }
 
       if (Complications != null) {
@@ -43,58 +49,74 @@
         let sin60 = 0.866;
 
         if (Complications.t != null) {
-          Complications.t.draw(
-            CenterX,CenterY-sin60*PlacementRadius,
-            ComplicationRadius, Settings
-          );
-        } else {
-          if (Complications.tl != null) {
-            Complications.tl.draw(
-              CenterX-sin30*PlacementRadius,CenterY-sin60*PlacementRadius,
+          try {
+            Complications.t.draw(
+              CenterX,CenterY-sin60*PlacementRadius,
               ComplicationRadius, Settings
             );
+          } catch (Signal) { print('Error drawing complication at position "t": ',Signal); }
+        } else {
+          if (Complications.tl != null) {
+            try {
+              Complications.tl.draw(
+                CenterX-sin30*PlacementRadius,CenterY-sin60*PlacementRadius,
+                ComplicationRadius, Settings
+          } catch (Signal) { print('Error drawing complication at position "tl": ',Signal); }
+              );
           }
 
           if (Complications.tr != null) {
-            Complications.tr.draw(
-              CenterX+sin30*PlacementRadius,CenterY-sin60*PlacementRadius,
-              ComplicationRadius, Settings
-            );
+            try {
+              Complications.tr.draw(
+                CenterX+sin30*PlacementRadius,CenterY-sin60*PlacementRadius,
+                ComplicationRadius, Settings
+              );
+            } catch (Signal) { print('Error drawing complication at position "tr": ',Signal); }
           }
         }
 
         if (Complications.l != null) {
-          Complications.l.draw(
-            CenterX-PlacementRadius,CenterY,
-            ComplicationRadius, Settings
-          );
+          try {
+            Complications.l.draw(
+              CenterX-PlacementRadius,CenterY,
+              ComplicationRadius, Settings
+            );
+          } catch (Signal) { print('Error drawing complication at position "l": ',Signal); }
         }
 
         if (Complications.r != null) {
-          Complications.r.draw(
-            CenterX+PlacementRadius,CenterY,
-            ComplicationRadius, Settings
-          );
+          try {
+            Complications.r.draw(
+              CenterX+PlacementRadius,CenterY,
+              ComplicationRadius, Settings
+            );
+          } catch (Signal) { print('Error drawing complication at position "r": ',Signal); }
         }
 
         if (Complications.b != null) {
-          Complications.b.draw(
-            CenterX,CenterY+sin60*PlacementRadius,
-            ComplicationRadius, Settings
-          );
-        } else {
-          if (Complications.bl != null) {
-            Complications.bl.draw(
-              CenterX-sin30*PlacementRadius,CenterY+sin60*PlacementRadius,
+          try {
+            Complications.b.draw(
+              CenterX,CenterY+sin60*PlacementRadius,
               ComplicationRadius, Settings
             );
+          } catch (Signal) { print('Error drawing complication at position "b": ',Signal); }
+        } else {
+          if (Complications.bl != null) {
+            try {
+              Complications.bl.draw(
+                CenterX-sin30*PlacementRadius,CenterY+sin60*PlacementRadius,
+                ComplicationRadius, Settings
+              );
+            } catch (Signal) { print('Error drawing complication at position "bl": ',Signal); }
           }
 
           if (Complications.br != null) {
-            Complications.br.draw(
-              CenterX+sin30*PlacementRadius,CenterY+sin60*PlacementRadius,
-              ComplicationRadius, Settings
-            );
+            try {
+              Complications.br.draw(
+                CenterX+sin30*PlacementRadius,CenterY+sin60*PlacementRadius,
+                ComplicationRadius, Settings
+              );
+            } catch (Signal) { print('Error drawing complication at position "br": ',Signal); }
           }
         }
       }
@@ -105,11 +127,13 @@
       let Minutes = now.getMinutes();
       let Seconds = now.getSeconds();
 
-      let withSeconds = (Settings.Seconds != null) && Bangle.isLCDOn();
-      ClockHands.draw(
-        Settings, CenterX,CenterY, outerRadius,
-        Hours,Minutes,(withSeconds ? Seconds : null)
-      );
+      try {
+        let withSeconds = (Settings.Seconds != null) && Bangle.isLCDOn();
+        ClockHands.draw(
+          Settings, CenterX,CenterY, outerRadius,
+          Hours,Minutes,(withSeconds ? Seconds : null)
+        );
+      } catch (Signal) { print('Error drawing clock hands: ',Signal); }
 
       let Period = (withSeconds ? 1000 : 60000);
 
@@ -117,7 +141,7 @@
       Timer = setTimeout(refreshDisplay,Pause);
     }
 
-    setTimeout(refreshDisplay, 500);               // enqueue first draw request
+    Timer = setTimeout(refreshDisplay, 500);       // enqueue first draw request
 
     Bangle.on('lcdPower', () => {
       if (Timer != null) { clearTimeout(Timer); Timer = undefined; }
